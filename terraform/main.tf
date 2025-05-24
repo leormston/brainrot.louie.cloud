7b37dabb5ab24d73095273df6e6e13c9f29b17e8
leormston <l.e.ormston@gmail.com>
2025-05-24 03:12:45 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,10 +83,17 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
   zone_id = data.aws_route53_zone.louie_cloud.zone_id
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
   ttl     = 60
 }
 
