1db41978c4fe96b40f0fee361fefa06279c82bb7
leormston <l.e.ormston@gmail.com>
2025-05-24 02:53:05 +0100

    pushing IAC

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+# S3 bucket for static site
+resource "aws_s3_bucket" "site" {
+  bucket = "brainrot.louie.cloud"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = "s3-brainrot-louie-cloud"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-brainrot-louie-cloud"
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  aliases = ["brainrot.louie.cloud"]
+}
+
+# ACM certificate (must be in us-east-1 for CloudFront)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+resource "aws_acm_certificate" "cert" {
+  provider          = aws.us_east_1
+  domain_name       = "brainrot.louie.cloud"
+  validation_method = "DNS"
+}
+
+# Route 53 DNS record
+data "aws_route53_zone" "louie_cloud" {
+  name = "louie.cloud."
+}
+
+resource "aws_route53_record" "brainrot" {
+  zone_id = data.aws_route53_zone.louie_cloud.zone_id
+  name    = "brainrot.louie.cloud"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
