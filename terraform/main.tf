8fd06c449bd29f00347ec97ae093bbfc21924d4c
leormston <l.e.ormston@gmail.com>
2025-05-24 02:53:28 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,26 @@
 # S3 bucket for static site
 resource "aws_s3_bucket" "site" {
   bucket = "brainrot.louie.cloud"
-  acl    = "public-read"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
   }
 }
 
@@ -36,6 +51,12 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 
   aliases = ["brainrot.louie.cloud"]
