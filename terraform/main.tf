a742b8ed957cce3273ec4f746690ec88ba217166
leormston <l.e.ormston@gmail.com>
2025-05-24 02:56:41 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
+    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method  = "sni-only"
   }
 
@@ -72,6 +72,27 @@ resource "aws_acm_certificate" "cert" {
   provider          = aws.us_east_1
   domain_name       = "brainrot.louie.cloud"
   validation_method = "DNS"
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  zone_id = data.aws_route53_zone.louie_cloud.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 # Route 53 DNS record
